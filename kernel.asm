4bee928eec897576d3b8204c917de0583c040cb2
coursework <coursework@coursework.csc.tntech.edu>
2022-09-15 10:18:28 -0500

    Adds basic iteration of assembly

--- kernel.asm
+++ kernel.asm
@@ -2,6 +2,33 @@ GLOBAL k_printstr
 
 k_printstr:
     push ebp
-    mov, esp
+    mov ebp, esp
     pushf
-    push eax ; what in God's name do I also need to add
+    push esi ; string pointer
+    push eax ; offset
+    push ebx ; row
+    push ecx ; column
+
+    mov esi, [ebp+12]
+    ; I think it's +12 because the string is the *first* thing into the function
+    mov ebx, [ebp+8]
+    mov ecx, [ebp+4]
+
+
+    loop:
+        cmp [STRING], 00 ; string is 0 bytes big left
+        je loop_end
+        cmp [STRING], [MAXMEMLOCATIONINVIDEO]
+        jg loop_end
+        movsb ; gets the character and puts it into mem for video and incs
+        mov WHITETEXTBLUEBACKGROUND, [CURMEMLOCATION]
+        inc [CURMEMLOCATION]
+        jmp loop
+    loop_end:
+        pop ecx
+        pop ebx
+        pop eax
+        pop esi
+        popf
+        pop ebp
+        ret
