48c90b902ca9ef067b89d24190a9cedffb02d42d
coursework <coursework@coursework.csc.tntech.edu>
2022-09-17 20:23:26 -0500

    Fixes a few issues? and removes placeholders

--- kernel.asm
+++ kernel.asm
@@ -1,6 +1,6 @@
 GLOBAL k_printstr
 
-vgaStart    DWORD PTR 0xB8000 H
+vgaStart    DWORD PTR 0xB8000h
 k_printstr:
     push ebp
     mov ebp, esp
@@ -12,20 +12,21 @@ k_printstr:
     push ecx ; column
 
     mov edi, vgaStart
-    mov esi, [ebp+12]
+    mov [esi], [ebp+12]
     ; I think it's +12 because the string is the *first* thing into the function
     mov ebx, [ebp+8]
     mov ecx, [ebp+4]
 
+   
 
     loop:
-        cmp [esi], 0 ; string is 0 bytes big left
+        cmp BYTE [esi], 0 ; string is 0 bytes big left
         je loop_end
-        cmp esi+eax, vgaStart+4000 ; 4000 = 25*80*2
+        cmp [esi+eax], [vgaStart+4000] ; 4000 = 25*80*2
         jg loop_end
         movsb ; gets the character and puts it into mem for video and incs
-        mov WHITETEXTBLUEBACKGROUND, [CURMEMLOCATION]
-        inc [CURMEMLOCATION]
+        mov 31h, [edi]
+        inc edi
         jmp loop
     loop_end:
         pop ecx
