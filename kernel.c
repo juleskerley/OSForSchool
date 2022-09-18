96a1edbf9419c15642d696e80fa01e028e6339da
coursework <coursework@coursework.csc.tntech.edu>
2022-09-17 21:16:17 -0500

    Correctly creates char pointer

--- kernel.c
+++ kernel.c
@@ -4,7 +4,7 @@ void k_printstr(char *string, int row, int col);
 
 
 int main(){
-    char *help = "hello world";
-    k_printstr(*help, 10, 10);
+    char string* = "hello world";
+    k_printstr(*string, 10, 10);
     return 0;
 }
