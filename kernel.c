d88457a3dbbd8a594b75f2d7d083dbd27b2eb8fa
coursework <coursework@coursework.csc.tntech.edu>
2022-09-18 00:28:31 -0500

    Completes a basic working border to specifications

--- kernel.c
+++ kernel.c
@@ -21,9 +21,12 @@ void k_clearscr(){
 void print_border(int start_row, int start_col, int end_row, int end_col){
     for (int i = 0; i < 25; i++){
         for (int j = 0; j < 80; j++){
-            // golly gee darn this is awful
             if (i == 0 && j == 0 || i == 24 && j == 0 || i == 0 && j == 79 || i == 24 && j == 79)
                 k_printstr("+", i, j);
+            else if (i == 0 || i == 24)
+                k_printstr("-", i, j);
+            else if (j == 0 || j == 79)
+                k_printstr("|", i, j);
         }
     }
 }
