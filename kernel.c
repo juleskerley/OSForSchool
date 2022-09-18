801a13d63215e3e3c0126beb91882ad0da217164
coursework <coursework@coursework.csc.tntech.edu>
2022-09-17 23:56:42 -0500

    Adds required code for assignment

--- kernel.c
+++ kernel.c
@@ -1,12 +1,12 @@
 void k_clearscr();
 void k_printstr(char *string, int row, int col);
-
+void run_test();
 
 
 int main(){
-    while(1){
     char *string = "hello world";
     k_printstr(string, 0, 0);
-    }
+    run_test();
+    while(1){} // This keeps the screen from flickering
     return 0;
 }
